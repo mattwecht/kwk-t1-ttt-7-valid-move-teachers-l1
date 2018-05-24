871cbae257eba40af05d660868eeccc3325cbf0a
mattwecht <mattwecht@gmail.com>
2018-05-24 13:36:19 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,11 @@
 # code your #valid_move? method here
-
+#returns true when move is valid, false when not valid 
+def valid_move(board, index)
+  if index.isbetween?(0,8)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+#returns true when position is not blank 
+def position_taken?(board, index)
+  board[index] != " " && board[index] != "" && board[index] != nil
+end
